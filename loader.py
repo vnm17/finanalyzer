5b404fb08e407755766710c8cadebf9f3b47d774
Viveka Mastandrea <viveka.mastandrea@gmail.com>
2017-08-01 10:01:04 -0700

    looking at uber expenses

--- loader.py
+++ loader.py
@@ -4,6 +4,7 @@ import os
 import _pickle as pickle
 from make_graphs import *
 from datetime import datetime
+from math import floor
 
 
 
@@ -17,19 +18,29 @@ from datetime import datetime
 
 def read_data():
     data = pandas.read_csv("data/transactions.csv")
-    data.columns = ["date", "description", "description", "amount",
+    data.columns = ["date", "description", "original_description", "amount",
                     "transaction_type","category","account_name","labels","notes"]
     data.date = data.date.apply(lambda d: datetime.strptime(d, "%m/%d/%Y")) # 7/26/2017
+    data.amount = data.amount.apply(lambda a: floor(a * 100))
     # print(data.columns)
     return data
 
+def get_date(s):
+    return datetime.strptime(s, "%m/%d/%Y")
+
 def loop(data):
-    categories = {}
+    total = 0
     for index,row in data.iterrows():
-        if row["category"] not in categories:
-            categories[row["category"]] = 0
-        categories[row["category"]] += 1
-    print(categories)
+        if row["date"] > get_date("06/09/2017"):
+            # print(row["description"])
+            if row["description"] == "Uber.com":
+                if row["transaction_type"] == "debit":
+                    total += row["amount"]
+                else:
+                    total -= row["amount"]
+    total = total / 100
+    print("Total spent on uber:",total)
+
 
 # separate credit from debit
 def separate(data):
@@ -41,7 +52,4 @@ def separate(data):
 
 if __name__ == "__main__":
     transactions = read_data()
-    print("Have data!")
-    purchases,credits = separate(transactions)
-    spending(purchases)
-    # pie_chart(purchases,"category")
+    loop(transactions)
